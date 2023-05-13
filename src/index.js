d41267fa04268b38de136b1d1e0a6ea5e4a8dd9b
joyce-madjos <madjos.joyce.arapan@gmail.com>
2023-05-13 18:46:37 +0800

    Created some contents and added scss

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import './style.scss';
+
+
 console.log("Meow!");
+
+function component() {
+    const element = document.createElement('div');
+    element.textContent = "Catmeow";
+    element.id = "element";
+    return element;
+  }
+  
+  document.body.appendChild(component());
