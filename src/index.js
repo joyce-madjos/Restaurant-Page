d0df62bc07d1109ac0da60abf0dad4320fe0e62b
joyce-madjos <madjos.joyce.arapan@gmail.com>
2023-05-14 12:44:35 +0800

    Fixed bug

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ let content = document.getElementById("content");
 
 console.log("Meow!");
 
-function component() {
-    const element = document.createElement('div');
-    element.textContent = "Catmeow";
-    element.id = "element";
-    return element;
-  }
+// function component() {
+//     const element = document.createElement('div');
+//     element.textContent = "Catmeow";
+//     element.id = "element";
+//     return element;
+//   }
   
-  content.appendChild(component());
-  content.appendChild(about());
+  // content.appendChild(component());
+  // content.appendChild(about());
